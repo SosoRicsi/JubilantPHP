d023fa2c05b3933db7afaff0a717d5e8cab2eb42
SosoRicsi <matekzsenikem@gmail.com>
2024-06-04 19:48:29 +0200

    Update settings

--- App/settings.php
+++ App/settings.php
@@ -8,40 +8,40 @@
                                                         // Texts & Errors \\
                                                         ####################
 
-    /* ------ PasswordHash ------ */
-    $passwordMinimumCharError       = "A jelszó hosszúságe nem megfelelő! A minimum hossz: ";
-    $passwordLeastOneUppercase      = "A jelszónak legalább egy nagybetűt kell tartalmaznia!";
-    $passwordLeastOneLowercase      = "A jelszónak legalább egy kisbetűt kell tartalmaznia!";
-    $passwordLeastOneNumber         = "A jelszónak legalább egy számot tartalmaznia kell!";
-    $passwordLeastOneSpecialChar    = "A jelszónak legalább egy speciális karaktert tartalmaznia kell!";
+                                                    /* ------ PasswordHash ------ */
+    $passwordMinimumCharError           = "A jelszó hosszúságe nem megfelelő! A minimum hossz: ";
+    $passwordLeastOneUppercase          = "A jelszónak legalább egy nagybetűt kell tartalmaznia!";
+    $passwordLeastOneLowercase          = "A jelszónak legalább egy kisbetűt kell tartalmaznia!";
+    $passwordLeastOneNumber             = "A jelszónak legalább egy számot tartalmaznia kell!";
+    $passwordLeastOneSpecialChar        = "A jelszónak legalább egy speciális karaktert tartalmaznia kell!";
 
-    /* ------ UserAuth ------ */
+                                                    /* ------ UserAuth ------ */
         /* registration */
-    $cantCreateUsersTable           = "Nem sikerült létrehozni a <i>felhasználók</i> táblát!";
-    $cantCreateLastloginsTable      = "Nem sikerült létrehozni az <i>új bejelentkezések</i> táblát!";
-    $emptyUsernameInput             = "A <i>felhasználónév</i> mező nem lehet üres! <br>";
-    $emptyEmailInput                = "Az <i>e-mail cím</i> mező nem lehet üres! <br>";
-    $emptyPasswordInput             = "A <i>jelszó</i> mező nem lehet üres! <br>";
-    $notValidEmail                  = "Nem érvényes <i>e-mail cím ($email)</i>! <br>";
-    $emailAlredyUsed                = "Az <i>e-mail cím ($email)</i> már használatban van! <br>";
-    $registeredSuccessfully         = "Sikeres regisztráció! <br>";
-    $registeredUnsuccessfully       = "Sikertelen regisztráció! <br>";
+    $cantCreateUsersTable               = "Nem sikerült létrehozni a <i>felhasználók</i> táblát!";
+    $cantCreateLastloginsTable          = "Nem sikerült létrehozni az <i>új bejelentkezések</i> táblát!";
+    $emptyUsernameInput                 = "A <i>felhasználónév</i> mező nem lehet üres! <br>";
+    $emptyEmailInput                    = "Az <i>e-mail cím</i> mező nem lehet üres! <br>";
+    $emptyPasswordInput                 = "A <i>jelszó</i> mező nem lehet üres! <br>";
+    $notValidEmail                      = "Nem érvényes <i>e-mail cím ($email)</i>! <br>";
+    $emailAlredyUsed                    = "Az <i>e-mail cím ($email)</i> már használatban van! <br>";
+    $registeredSuccessfully             = "Sikeres regisztráció! <br>";
+    $registeredUnsuccessfully           = "Sikertelen regisztráció! <br>";
 
         /* login */
-    $invalidPassword                = "Helytelen jelszó! <br>";
+    $invalidPassword                    = "Helytelen jelszó! <br>";
     
         /* passwordchange */
-    $passwordSuccessfullyChanged    = "A jelszó sikeresen megváltoztatva! <br>";
-    $passwordUnsuccessfullyChanged  = "A jelszót nem sikerült megváltoztatni! Kérjük próbálja újra később! <br>";
+    $passwordSuccessfullyChanged        = "A jelszó sikeresen megváltoztatva! <br>";
+    $passwordUnsuccessfullyChanged      = "A jelszót nem sikerült megváltoztatni! Kérjük próbálja újra később! <br>";
 
         /* confirm user */
-    $userConfirmedSuccessfully      = "A felhasználói fiók sikeresen megerősítésre került! <br>";
-    $userConfirmedUnsuccessfully    = "A felhasználói fiókot nem sikerült megerősíteni!";
+    $userConfirmedSuccessfully          = "A felhasználói fiók sikeresen megerősítésre került! <br>";
+    $userConfirmedUnsuccessfully        = "A felhasználói fiókot nem sikerült megerősíteni!";
     
-    $userNotFound                   = "Nem található felhasználói fiók! <br>";
-    $invalidPHPsessionID            = "A küldött <i>php session id</i> nem egyezik meg a valós értékkel! <br>";
+    $userNotFound                       = "Nem található felhasználói fiók! <br>";
+    $invalidPHPsessionID                = "A küldött <i>php session id</i> nem egyezik meg a valós értékkel! <br>";
 
-    /* ------ FileUpload ------ */
-    $cantCreateFileUploadsTableError = "Nem sikerült létrehozni a <i>feltöltött fájlok</i> táblát!";
+                                                    /* ------ FileUpload ------ */
+    $cantCreateFileUploadsTableError    = "Nem sikerült létrehozni a <i>feltöltött fájlok</i> táblát!";
 
 ?>
