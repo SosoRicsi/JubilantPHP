11c22e74168f8a1973c3511e0957eefaaf297947
SosoRicsi <matekzsenikem@gmail.com>
2024-06-26 00:45:10 +0200

    Bug fixes

--- App/public/routes.php
+++ App/public/routes.php
@@ -14,9 +14,6 @@
     $Router = new Router();
 
     $Router->get('/', [IndexController::class, 'index']);
-    $Router->get('/{name}', function ($name) {
-        echo $name;
-    });
 
     $Router->add404Handler(function () {
         $Template = new Template(__DIR__.'/MVC/views/404.blade.php');
