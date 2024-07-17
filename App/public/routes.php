47112bcb55451843a5881e2c0a737ffb7158ad54
SosoRicsi <98759127+SosoRicsi@users.noreply.github.com>
2024-07-17 03:04:06 +0200

    Update routes.php

--- App/public/routes.php
+++ App/public/routes.php
@@ -11,9 +11,23 @@
     use App\Controllers\IndexController;
     use App\Middlewares\Auth;
 
+    //use the router (for groups)
+    //$router = $Application->router;
+
     //with auth middleware:
     //$Application->router->get('/', [IndexController::class, 'index'], [Auth::class]);
     $Application->router->get('/', [IndexController::class, 'index']);
+
+    //redirect the user from 'alma' to the main page
+    //$Application->router->redirect('/alma','/');
+
+    //groups:
+    /* $Application->router->group('/user', [Auth::class], function ($router) {
+        $router->get('/login', function () {
+            echo "Login page";
+        }, [LoginStatus::class]);
+        $router->get('/register', [RegisterController::class, 'index']);
+    }); */
 
     $Application->router->add404Handler(function () {
         $Template = new Template(__DIR__.'/MVC/views/404.blade.php');
