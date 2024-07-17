a946ee9ab3e959630edd42868ce5d672cc195d94
SosoRicsi <98759127+SosoRicsi@users.noreply.github.com>
2024-07-17 03:01:29 +0200

    Update index.php

--- App/public/index.php
+++ App/public/index.php
@@ -6,10 +6,15 @@
     */
 
     require __DIR__.'/../settings.php';
+
     use Jubilant\Application;
+    use Jubilant\Router;
+
+    /* Set up the router */
+    $router = new Router();
 
     /* Set the application */
-    $Application = new Application();
+    $Application = new Application($router);
 
     /* Set the paths */
     require __DIR__.'/routes.php';
